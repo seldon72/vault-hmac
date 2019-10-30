8f8e7c90c9fde6855cfa7225dfb95ccb9ac0bda9
Paco Ulloa <ulloa.paco@gmail.com>
2019-10-30 11:04:36 -0600

    Syntax error

--- bash/get_token.sh
+++ bash/get_token.sh
@@ -2,7 +2,7 @@
 
 PKCS=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/pkcs7 | tr -d '\n')
 
-payload = $(cat <<-EOF 
+payload=$(cat <<-EOF 
     {
         "role":"DOU-role",
         "pkcs7":"$PKCS",
